7c103bfc011f14c4f58ea8eb5e66305608754bf3
d-sar <damrisara1@gmail.com>
2025-10-13 10:36:59 +0100

    first commit

---
+++ customer-service/src/main/java/com/enset/customerservice/CustomerServiceApplication.java
@@ -0,0 +1,36 @@
+package com.enset.customerservice;
+
+import com.enset.customerservice.entities.Customer;
+import com.enset.customerservice.repo.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CustomerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerServiceApplication.class, args);
+    }
+
+
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder()
+                            .name("sara")
+                            .email("sara@gmail.com")
+                            .build());
+            customerRepository.save(Customer.builder()
+                    .name("hamza")
+                    .email("hamza@gmail.com")
+                    .build());
+            customerRepository.save(Customer.builder()
+                    .name("fati")
+                    .email("fati@gmail.com")
+                    .build());
+            customerRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
